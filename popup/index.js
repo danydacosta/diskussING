5dfab49c44e0990291c4e1bb32e8ad1145ac250a
danydacosta <danydc@comac.ch>
2017-09-12 09:22:15 +0200

    Added connection UI

--- popup/index.js
+++ popup/index.js
@@ -2,6 +2,11 @@ $('.toggle').click(() => {
     $('.blur').toggleClass('displaynone');
 });
 
+$('.connectbutton').click(() => {
+    $('.main-container').toggleClass('displaynone');
+    $('.connect-container').toggleClass('displaynone');
+});
+
 $('.addbutton').click(() => {
     $('.blur').toggleClass('displaynone');
     $('.modal').toggleClass('displaynone');
